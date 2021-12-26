f03cab445e4077730a056f47553c6c51126178b1
gaganj007 <gaganj2002@gmail.com>
2021-12-26 01:52:42 -0500

    Outputting data

--- src/components/CategoryComponent.js
+++ src/components/CategoryComponent.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import FunctionClick from './FunctionClickComponent';
+import Data from "./test.json";
 
 function Category(){
     return(
-        <div class="Category">
-        <p>Category
-        </p>
+        <div class="NEw">
+            <div className="posts">
+                
+                    <div>
+                        {Data.map((userInfo)=> {
+                            return (
+                                <h4>
+                                    <h4>{userInfo.paths['/Organizations'].get.responses[200].examples['application/json'][0].charityName}</h4>
+                                    <h4>{userInfo.paths['/Organizations'].get.responses[200].examples['application/json'][0].mission}</h4>
+                                    <h4>{userInfo.paths['/Organizations'].get.responses[200].examples['application/json'][0].websiteURL}</h4>
+                                    <h4>{userInfo.paths['/Organizations'].get.responses[200].examples['application/json'][0].donationAddress.city}</h4>
+                                     <h4>US</h4>
+                                    <h4>{userInfo.paths['/Organizations'].get.responses[200].examples['application/json'][0].donationAddress.postalCode}</h4>
+                                    
+                                </h4>
+                            
+                            
+                            )
+                               
+                        })}
+                    </div>
+            
+            </div>
+        
         <FunctionClick/>
         </div>
     )
